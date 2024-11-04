0ba45655ddc1d89c3747641b33c4a3f5e6e91c23
Alok Rawat <alokrawatt@gmail.com>
2024-11-05 00:00:41 +0530

    made required changes

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header className="bg-blue-600 text-white p-4">
+            <h1 className="text-3xl font-bold text-center">SVG to Code Converter</h1>
+            <p className="text-center">Convert your SVG files to code effortlessly!</p>
+        </header>
+    );
+};
+
+export default Header; 
